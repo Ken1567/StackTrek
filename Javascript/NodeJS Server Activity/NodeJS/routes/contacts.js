c2ffa5997fc266660d407bfccaec26d560b0b2fb
Ken1567 <107854367+Ken1567@users.noreply.github.com>
2022-08-23 21:13:02 +0800

    Delete Javascript/NodeJS Server Activity/NodeJS directory

--- Javascript/NodeJS Server Activity/NodeJS/routes/contacts.js
+++
@@ -1,7 +0,0 @@
-const contactList = {
-    email: "garrovillomanuelkennon@gmail.com",
-    number: "09456292038"
-}
-// document.getElementById("email").innerHTML = contactList.email;
-// document.getElementById("number").innerHTML = contactList.number;
-module.exports = contactList
